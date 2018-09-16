724e8e5db913a74a9ba87eb290d3bee70175e419
Joshua Castillo <joshua.castillo@sap.com>
2018-09-15 22:21:36 -0500

    Add scaffoldinging for network related tests and for developer code specific tests

---
+++ master.js
@@ -0,0 +1,10 @@
+import '.lib/performance.js'
+
+// run all these tests together
+// look at tags on functions and run if they have the tag end2end
+export runAllTests function() {
+    //logic to only run the two above based on tags
+    // loops through all tests
+}
+
+// function to run only one test
